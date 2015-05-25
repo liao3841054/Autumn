6ab3866af2a116d192df0e8ecbe20cf35262118a
Ding Soung <DingSoung@gmail.com>
2015-05-25 13:50:28 +0800

    简单的swift 单例

---
+++ summer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  summer
+//
+//  Created by Ding Soung on 15/5/25.
+//  Copyright (c) 2015年 Ding Soung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		println(ShareData.instance.index)
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
