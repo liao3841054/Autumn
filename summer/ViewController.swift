e2b251d86417ed3cb956f28b0c5f52308aceebc6
Ding Soung <DingSoung@gmail.com>
2015-05-31 17:26:48 +0800

    RAC action，     需要聚合信号

--- summer/ViewController.swift
+++ summer/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class ViewController: UIViewController {
-
+	
 	@IBOutlet weak var name: UITextField!
 	@IBOutlet weak var pwssword: UITextField!
 	@IBOutlet weak var sigIn: UIButton!
@@ -40,19 +41,61 @@ class ViewController: UIViewController {
 		self.pwssword.addTarget(self, action: "passwdChanged:", forControlEvents: UIControlEvents.EditingChanged)
 		self.name.rac_textSignal().filter { (dat) -> Bool in
 			return (dat as! NSString).length > 3
-		}.subscribeNext { (dat) -> Void in
-			println(dat)
+			}.subscribeNext { (dat) -> Void in
+				println(dat)
 		}
+		self.pwssword.rac_textSignal()
+			.map { (dat) -> AnyObject! in
+				return (dat as! NSString).length
+			}.filter { (dat) -> Bool in
+				return (dat as! NSNumber).integerValue > 3
+			}.subscribeNext { (dat) -> Void in
+				println(dat)
+		}
+		self.sigIn.rac_signalForControlEvents(UIControlEvents.TouchUpInside).subscribeNext { (dat) -> Void in
+			println("button clinked")
+		}
+		
+		var vaidNameSignal = self.name.rac_textSignal().map { (dat) -> AnyObject! in
+			return count(dat as! String) > 3
+		}
+		var vaidPasswdSignal = self.pwssword.rac_textSignal().map { (dat) -> AnyObject! in
+			return count(dat as! String) > 4
+		}
+		//		var signUpSignal = RACSignal.combineLatest([vaidNameSignal,vaidPasswdSignal]).map { (dat) -> AnyObject! in
+		//			return (self.vaidNameSignal as! Bool)
+		//		}
+		
+		
+		
+		#if false //RAC 宏定义在swift中不可用
+			RAC(self.name.backgroundColor) = vaidNameSignal.map({ (dat) -> AnyObject! in
+			return (dat as! Bool) ? UIColor.clearColor() : UIColor.whiteColor()
+			})
+			#else
+			vaidNameSignal.map { (dat) -> AnyObject! in
+				return (dat as! Bool) ? UIColor.clearColor() : UIColor.yellowColor()
+				}.subscribeNext { (dat) -> Void in
+					self.name.backgroundColor = (dat as! UIColor)
+			}
+		#endif
+		vaidPasswdSignal.map { (dat) -> AnyObject! in
+			return (dat as! Bool) ? UIColor.clearColor() : UIColor.yellowColor()
+			}.subscribeNext { (dat) -> Void in
+				self.pwssword.backgroundColor = (dat as! UIColor)
+		}
+		
+		
+		
+		
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 	}
 	
 	
 	private func updateUIState() {
-		self.name.backgroundColor = self.nameIsValid ? UIColor.clearColor() : UIColor.yellowColor()
-		self.pwssword.backgroundColor = self.passwordISValid ? UIColor.clearColor() : UIColor.yellowColor()
 		self.sigIn.enabled = self.nameIsValid && self.passwordISValid
 	}
 	func nameChanged(sendr:AnyObject){
@@ -63,7 +106,7 @@ class ViewController: UIViewController {
 		self.passwordISValid = count(self.pwssword.text) > 3
 		self.updateUIState()
 	}
-
-
+	
+	
 }
 
